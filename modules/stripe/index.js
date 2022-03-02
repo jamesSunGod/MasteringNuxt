b06bf4930793bc1b988ffbcb1ae5a5553099b204
jamesSunGod <james@sungod.co>
2022-03-02 15:25:38 +0000

    Adding webhook URL

--- modules/stripe/index.js
+++ modules/stripe/index.js
@@ -14,6 +14,13 @@ export default function() {
     app.use('/api/stripe/create-session', createSession)
   })
 
+  this.nuxt.hook('render:setupMiddleware', (app) => {
+    app.use('/hooks/stripe/', (req, res, next) => {
+      const meta = req.body.data.object.metadata
+      res.end(`${meta.indentityId} booked ${meta.homeId}`)
+    })
+  })
+
   async function createSession(req, res) {
     const body = req.body
     if(!body || !body.homeId || !body.start || !body.end || !body.start >= body.end) {
@@ -23,6 +30,12 @@ export default function() {
     const home = (await apis.homes.get(body.homeId)).json
     const nights = (body.end - body.start) / 86400
     const session = await stripe.checkout.sessions.create({
+      metadata: {
+        identityId: req.identity.id,
+        homeId: body.homeId,
+        start: body.start,
+        end: body.end
+      },
       payment_method_types: ['card'],
       mode: 'payment',
       success_url: `${rootUrl}/home/${body.homeId}?result=success`,
